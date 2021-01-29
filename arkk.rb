f4bd7f9039c1b857b477647ccea67aa2dc6c1b0a
Jordan Reed <crasinator@gmail.com>
2021-01-28 23:58:03 -0500

    Added Methods for Stock API

--- arkk.rb
+++ arkk.rb
@@ -1,3 +1,94 @@
+require 'rubygems'
+require 'httparty'
+
+require 'json'
+
+class Twelvedata
+    include HTTParty
+    base_uri "api.twelvedata.com/"
+
+    def arkk
+       var = self.class.get ('/time_series?symbol=ARKK&interval=1day&apikey=716b4530d098461283fb2c02408a6bb8')
+    end  
+
+    def arkk_symbol
+        self.arkk["meta"]["symbol"]
+    end 
+
+    def arkk_price
+        self.arkk["values"][0]["close"]
+    end 
+
+    def tsla
+        var = self.class.get ('/time_series?symbol=TSLA&interval=1day&apikey=716b4530d098461283fb2c02408a6bb8')
+    end  
+
+    def tsla_symbol
+        self.tsla["meta"]["symbol"]
+    end 
+
+    def tsla_price
+        self.tsla["values"][0]["close"]
+    end 
+
+    def pypl
+        var = self.class.get ('/time_series?symbol=PYPL&interval=1day&apikey=716b4530d098461283fb2c02408a6bb8')
+    end  
+
+    def pypl_symbol
+        self.pypl["meta"]["symbol"]
+    end 
+
+end 
+ween.pypl["meta"]["symbol"]
+ween.tsla["meta"]["symbol"]
+# symbol name 
+# ween.arkk["meta"]["symbol"]
+# => "ARKK" 
+
+# security type
+# ween.arkk["meta"]["type"]
+# => "ETF"
+
+# ARKK at close
+# ween.arkk["values"][0]["close"]
+# => "140.38240" 
+
+ween = Twelvedata.new
+
+ween.map.do |x| 
+x.map.do |y| 
+if y.include?("ETF")
+    return y
+end 
+end 
+
+
+
+ween.arkk.find {|k, v| k == "meta" }
+
+ween{"meta"}{"type"}
+
+balls.find {|k| k == "type"}
+
+    def tsla
+        self.class.get ('/time_series?symbol=ARKK&interval=1day&apikey=716b4530d098461283fb2c02408a6bb8')
+    end 
+
+    def tsla
+    self.class.get ('/time_series?symbol=ARKK&interval=1day&apikey=716b4530d098461283fb2c02408a6bb8')
+    end 
+
+    def market? 
+
+    end 
+    
+
+    def 
+end 
+
+ween = Twelvedata.new
+puts ween.arkk
 class Arkk
     # METHODS THAT NEED TO BE DEFINED 
     #
@@ -20,6 +111,33 @@ class Arkk
     # Arkk.(increased/decreased (by $x))
     # 
     # also need stock, quote and fortune api
+
+    #Stock API 
+        #https://api.twelvedata.com/time_series?symbol=ARKK,TSLA,PYPL&interval=1day&apikey=716b4530d098461283fb2c02408a6bb8
+            #This has json data for $ARKK, $TSLA, $PYPL
+            #Can change the ticker by changing the value before comma (all caps)
+            #Can change the interval of refresh - time frame (e.g. 1day, 1week, 1month)
+            #can show hi/ low for day 
+            #  "ARKK": {
+            # "meta": {
+            #     "symbol": "ARKK",
+            #     "interval": "1day",
+            #     "currency": "USD",
+            #     "exchange_timezone": "America/New_York",
+            #     "exchange": "NYSE",
+            #     "type": "ETF"
+            # },
+            # "values": [
+            #     {
+            #     "datetime": "2021-01-26",
+            #     "open": "146.66000",
+            #     "high": "147.34000",
+            #     "low": "141.73000",
+            #     "close": "141.84000",
+            #     "volume": "8014706"
+            #     },
+            #     {
+
 
     def initialize(doc)
         @arkk_share_price #defined
@@ -49,9 +167,9 @@ class Arkk
     def call 
         puts "Welcome to Ark Tracker"
         puts    "in Cathie we trust"
-        if Arkk.market? == false 
-            puts  "the NYSE is currently closed" 
-        else
+        # if Arkk.market? == false 
+        #     puts  "the NYSE is currently closed" 
+        # else
             puts "$ARKK for #{todays_date}"
             puts "#{@arkk_share_price} per share"
                      #//finds the current share price from robinhood api 
@@ -59,7 +177,7 @@ class Arkk
                      #//finds the current intraday percentage chage for #{todays date}
             puts "ARKK intraday tracker availble for #{documents_week}"
                      #//goes thru the fold of exiting trade documents 
-        end
+        # end
         if tesla? == false             
             puts "there have been no $TSLA trades within ARK for #{documents_week}"
         else 
@@ -71,6 +189,9 @@ class Arkk
     end
 
     def market? 
+        if weekend || holiday || weekend 
+
+
         #//return t/f to check if market is open 
         #//returns f when NYSE is closed (holidays/weekends/afterhours)
     end 
